3060e6530b69d3b21b2e06892327bc98dffda238
Leonardo Schimpf Machado <leonardo.machado@radixeng.com.br>
2018-10-22 17:13:59 -0300

    Começando Http

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}, 
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent}
+];
+
+@NgModule({
+  imports: [ 
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )        
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {
+}
